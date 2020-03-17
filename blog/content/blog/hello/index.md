ca619d0e396be3e9f73bb5ed45a93c8202876b0c
yuu <bss@bss-mbp.local>
2020-03-17 19:46:18 +0900

    Gatsby init

---
+++ blog/content/blog/hello/index.md
@@ -0,0 +1,7 @@
+---
+title: "Hello World"
+date: "2020-03-17"
+description: "Gatsbyでブログ始めました！"
+---
+
+Gatsbyでブログ始めました！
---
+++ blog/gatsby-config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  siteMetadata: {
+    title: `Inquisitiveness`,
+    author: {
+      name: `Yuki Uehara`,
+      summary: `東京に行くと25歳と言われる地方学生エンジニア`,
+    },
+    description: `地方で生活している学生の日々勉強した内容や参加したイベント情報など`,
+    siteUrl: `https://blog.ue-y.me/`,
+    social: {
+      twitter: `psnzbss`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-114688805-9`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Inquisitiveness`,
+        short_name: `Inquisitiveness`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/siteicon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
