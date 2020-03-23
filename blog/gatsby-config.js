d0091059cb841ca735822dfca6230118106c8f14
yuu <uypsbss@gmail.com>
2020-03-23 13:51:28 +0900

    USE Prism.js

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -30,6 +30,17 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-code-titles`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: true,
+              noInlineHighlight: false,
+            },
+          },
           {
             resolve: `gatsby-remark-images`,
             options: {
