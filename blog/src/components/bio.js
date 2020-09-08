adba54a2e1cc7bce8d3eadcd214de5a6ed2f5549
yuu <uypsbss@gmail.com>
2020-09-08 22:31:32 +0900

    stayled toppage bio picture

--- blog/src/components/bio.js
+++ blog/src/components/bio.js
@@ -41,7 +41,7 @@ const Bio = () => {
       <Image
         fixed={data.avatar.childImageSharp.fixed}
         alt={author.name}
-        className="flex my-0 mx-auto"
+        className="block my-0 mx-auto rounded-full"
       />
       <p>
         Written by <strong>{author.name}</strong>(<a href={`https://twitter.com/${social.twitter}`}>@psnzbss</a>)
