c1415d213d8879ab0d1d456f9f7832f39738f085
yuu <uypsbss@gmail.com>
2020-09-13 02:49:01 +0900

    style use svg icon

--- blog/src/components/dark-mode.js
+++ blog/src/components/dark-mode.js
@@ -1,19 +1,20 @@
 import React from "react"
 import useDarkMode from "use-dark-mode"
-
 import Toggle from "./toggle"
+import sunSvg from "../../static/img/sun.svg"
+import moonSvg from "../../static/img/moon.svg"
 
 const DarkModeToggle = () => {
   const darkMode = useDarkMode(false);
 
   return (
-    <div className="dark-mode-toggle">
+    <div className="">
       <button type="button" onClick={darkMode.disable}>
-        ☀
+        <img src={sunSvg} width="25px"></img>
       </button>
       <Toggle checked={darkMode.value} onChange={darkMode.toggle} />
       <button type="button" onClick={darkMode.enable}>
-        ☾
+        <img src={moonSvg} width="25px"></img>
       </button>
     </div>
   )
