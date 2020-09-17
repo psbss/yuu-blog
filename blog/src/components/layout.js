022899384e4d6a0a9517fa464e0ed77b8f377eb5
yuu <uypsbss@gmail.com>
2020-09-17 23:02:46 +0900

    style fix top page

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -1,16 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
-
-import { rhythm, scale } from "../utils/typography"
 import Bio from "../components/bio"
 import DarkModeToggle from "./dark-mode"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
+const Layout = ({ title, children }) => {
+  <div className="container mx-auto">
+    <header class="mb-6 mx-2 mt-4">
       <div className="header">
         <h1 className="text-5xl">
           <Link>
@@ -19,30 +14,13 @@ const Layout = ({ location, title, children }) => {
         </h1>
         <DarkModeToggle />
       </div>
-    )
-  } else {
-    header = (
-      <div className="header">
-        <h1>
-          <Link>
-            {title}
-          </Link>
-        </h1>
-        <DarkModeToggle />
-      </div>
-    )
-  }
-
-  return (
-    <div className="container mx-auto">
-      <header class="mb-6 mx-2 mt-4">{header}</header>
-      <div className="mx-4 flex">
-        <Bio />
-        <main className="w-2/3">{children}</main>
-      </div>
-      <footer className="mt-6 mb-4 text-center">Yuki Uehara , {new Date().getFullYear()}</footer>
+    </header>
+    <div className="mx-4 flex">
+      <Bio />
+      <main className="w-2/3">{children}</main>
     </div>
-  )
+    <footer className="mt-6 mb-4 text-center">Yuki Uehara , {new Date().getFullYear()}</footer>
+  </div>
 }
 
 export default Layout
