39b1d86adf943cc21573b1be249355955e3edfd6
yuu <uypsbss@gmail.com>
2020-09-24 23:58:23 +0900

    style typography

--- blog/src/utils/typography.js
+++ blog/src/utils/typography.js
@@ -1,18 +1,16 @@
 import Typography from "typography"
-import Theme from "typography-theme-stow-lake"
 
-Theme.googleFonts.push({
-  name: "Noto+Sans+JP",
-  styles: ["400"],
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Georgia", "serif"],
 })
-Theme.bodyFontFamily = ["Noto Sans JP", "Roboto", "serif"]
-const typography = new Typography(Theme)
-
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
-}
-
 export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -16,4 +16,7 @@ module.exports = {
   },
   variants: {},
   plugins: [],
+  corePlugins: {
+    fontFamily: false,
+  }
 }
